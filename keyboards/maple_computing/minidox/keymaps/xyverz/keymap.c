4f14a1ab124b408258f79c4668469251c25f6fd7
Ian Sterling <ian@MWNQXLGXJGN.tld>
2025-05-08 14:49:29 -0700

    More code tidy-up.

--- keyboards/maple_computing/minidox/keymaps/xyverz/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/xyverz/keymap.c
@@ -1,10 +1,10 @@
 #include QMK_KEYBOARD_H
 
 // Layer Names Here.
-#define _DVORAK 0 // Base Layer
-#define _LOWER 1 // LOWER Layer
-#define _RAISE 2 // RAISE Layer
-#define _ADJUST 3 // ADJUST Layer
+#define DVORAK 0 // Base Layer
+#define LOWER 1 // LOWER Layer
+#define RAISE 2 // RAISE Layer
+#define ADJUST 3 // ADJUST Layer
 
 // Macros Here.
 #define CTLSCLN LCTL_T(KC_SCLN)
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_DVORAK] = LAYOUT_split_3x5_3(
+[DVORAK] = LAYOUT_split_3x5_3(
   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,       KC_F,    KC_G,    KC_C,    KC_R,    KC_L,
   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,       KC_D,    KC_H,    KC_T,    KC_N,    KC_S,
   CTLSCLN, ALTQ,    GUIJ,    KC_K,    KC_X,       KC_B,    KC_M,    GUIW,    ALTV,    RCTLZED,
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_LOWER] = LAYOUT_split_3x5_3(
+[LOWER] = LAYOUT_split_3x5_3(
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
   KC_CAPS, _______, KC_UP,   _______, KC_HOME,    KC_PGUP, KC_PLUS, KC_LCBR, KC_RCBR, KC_UNDS,
   KC_TILD, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,     KC_PGDN, KC_MUTE, KC_VOLD, KC_VOLU, KC_SLSH,
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_RAISE] = LAYOUT_split_3x5_3(
+[RAISE] = LAYOUT_split_3x5_3(
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
   KC_CAPS, _______, KC_UP,   _______, KC_HOME,    KC_PGUP, KC_EQL,  KC_LBRC, KC_RBRC, KC_MINS,
   KC_GRV,  KC_LEFT, KC_DOWN, KC_RGHT, KC_END,     KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_QUES,
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_ADJUST] =  LAYOUT_split_3x5_3(
+[ADJUST] =  LAYOUT_split_3x5_3(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_UP,   KC_F9,   KC_F10,
   KC_F11,  QK_BOOT, _______, _______, _______,    _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_F12,
   _______, _______, _______, _______, _______,    QK_BOOT, _______, _______, _______, _______,
