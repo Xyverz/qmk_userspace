57769a047f0e793a59c9ee2b0932d08804fa2228
Xyverz <xyverz@gmail.com>
2025-11-22 14:05:21 -0800

    Update keymap.c

--- keyboards/kinesis/keymaps/xyverz/keymap.c
+++ keyboards/kinesis/keymaps/xyverz/keymap.c
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     KC_GRV,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLNS, KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-              KC_LBRC,  KC_RBRC,  KC_LEFT,  KC_RGHT,                                                                                       KC_UP,    KC_DOWN,  KC_SLSH,  KC_EQL,
+              KC_EQL,   KC_MINS,  KC_LEFT,  KC_RGHT,                                                                                       KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
                                                       KC_LCTL,  KC_LALT,                                               KC_RGUI,  KC_RCTL,
                                                                 KC_LGUI,                                               KC_RGUI,
                                             KC_BSPC,  KC_DEL,   MO(2),                                                 MO(2),    KC_ENT,   KC_SPC
